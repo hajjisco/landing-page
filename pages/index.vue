35dba2afe39a8d69d008768eefb6b015193d9ed3
hajjisco <hajjisco12@gmail.com>
2024-01-22 12:41:11 +0100

    FILE

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template> 
+<div>{{ count }}</div>
+    <button @click="increment" class="p-3" >increase</button>
+    <br>
+    <button @click="decrement" class="p-3 bg-black text-white">decrease</button>
+    </template>
+    <script>
+        export default {
+            data() {
+                return {
+                    count: 0
+                }
+            },
+            methods: {
+                increment(){
+                    this.count++
+                },
+                decrement(){
+                    this.count--
+                }
+            },
+            
+        }
+    </script>
